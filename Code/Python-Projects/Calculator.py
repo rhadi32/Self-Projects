00133538706ed07c24062164d251d03b67ec4c28
rhadi32 <ramazanih22@gmail.com>
2024-04-30 19:47:00 -0500

    New Project

---
+++ Code/Python-Projects/Calculator.py
@@ -0,0 +1,54 @@
+# Author: Hadi Ramazani
+# Date: Apr 30,2024
+
+def calculate(n1,n2,op):
+    if op == "+":
+        result = n1+n2
+    elif op == "-":
+        result = n1-n2
+    elif op == "*":
+        result = n1*n2
+    elif op == "/":
+        result = n1/n2
+    elif op == "^":
+        result = n1**n2
+    elif op == "**":
+        result = n1**2        
+
+    else:
+        raise ValueError ("Invalid Operator")
+    if result.is_integer():
+        result = int(result)
+    
+    return result
+
+
+
+continue_calculating = True
+result = None  # To store the result of the last operation 
+
+while continue_calculating:
+    if result is None:
+        number1 = float(input("Enter a number: "))
+    else:
+        number1 = result
+    
+    op = input("Select an operator (+, -, *, /, ^, **) or enter 'q' to quit: ")
+
+    if op.lower() == 'q':
+        break
+    number2 = float(input("Enter another number: "))
+    
+    try:
+        result = calculate(number1, number2, op)
+        print('=', result)
+    except ValueError as e:
+        print(e)
+        result = None
+    
+print("Calculator session ended.")
+
+
+   
+    
+
