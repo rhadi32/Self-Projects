ec966f8b56c5fcc33e3110a7e1b210ac5b736213
rhadi32 <ramazanih22@gmail.com>
2024-05-04 21:22:32 -0500

    New changes

--- Code/Python-Projects/Number_Table.py
+++ Code/Python-Projects/Number_Table.py
@@ -1,11 +1,7 @@
-number = (int(input('Enter a number: ')))
-for i in range(1, 13):
-    if number in range(1, 13):
-        print(number, 'x', i, ': ', number*i)  
-else:   
-    print('out of range')  
-
-
-def main():
-     
-# end main
+while True:
+    number = (int(input('Enter a number: ')))
+    for i in range(1, 13):
+        if number not in range(1, 13):
+            print('out of range')
+        else:
+            print(number, 'x', i, ': ', number*i)
